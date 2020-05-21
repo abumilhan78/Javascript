de63b0220037536bd3098adfade91c7560fac822
abumilhan78 <abumilhan78@gmail.com>
2020-05-21 23:24:41 +0700

    some file refactored

--- 2kubus.js
+++ 2kubus.js
@@ -1,11 +1,5 @@
 function JumlahVolumeDuaKubus(a, b) {
-	var total, Vol1, vol2;
-
-	vol1 = a*a*a;
-	vol2 = b*b*b;
-
-	total = vol1+vol2;
-
-	return total;
+	
+	return a*a*a + b*b*b;
 }
 alert(JumlahVolumeDuaKubus(5,3));
